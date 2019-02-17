324f03eb4ebbbc2279a18a7f7c9218d93bfc1bd3
yesbee <premsagar0731@gmail.com>
2019-02-17 14:09:24 -0600

    intial commit

---
+++ src/main/java/com/yesbee/moviecatalogservice/controller/MovieCatalogController.java
@@ -0,0 +1,21 @@
+package com.yesbee.moviecatalogservice.controller;
+
+import com.yesbee.moviecatalogservice.domain.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogController {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        return Collections.singletonList(
+                new CatalogItem("Titanic", "Romantic genre", 5)
+        );
+    }
+}
